b84070c925bd9179196dbeeef15f418d52d1f09e
Gimmy <gimmyarazafimbelo2@gmail.com>
2018-04-11 09:59:07 +0300

    Update part1 and add part2

---
+++ Projet-Naina-S6-Part2/src/gtt/model/dao/GenericDAO.java
@@ -0,0 +1,56 @@
+package gtt.model.dao;
+
+import java.util.List;
+
+import gtt.model.BaseModel;
+import gtt.model.TableCondition;
+
+public class GenericDAO implements InterfaceDAO {
+	
+	// ATTRIBUTES :
+	
+	
+	
+	// CONSTRUCTS :
+	
+	
+	
+	// INHERITED METHODS :
+
+	@Override
+	public BaseModel save(BaseModel model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BaseModel update(BaseModel model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(TableCondition condition) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BaseModel findById(TableCondition condition) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BaseModel> findAll(TableCondition condition) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BaseModel> findAll(int page, int row, TableCondition condition) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
