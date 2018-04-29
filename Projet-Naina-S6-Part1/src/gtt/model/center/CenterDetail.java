22f4800be29872d9d8fd60f286462a751b4fb6b3
TotalInferno4 <tefyrak@gmail.com>
2018-04-29 12:39:34 +0300

    add model.center.CenterDetail.java

---
+++ Projet-Naina-S6-Part1/src/gtt/model/center/CenterDetail.java
@@ -0,0 +1,85 @@
+package gtt.model.center;
+
+import gtt.model.BaseModel;
+import gtt.model.ModelException;
+
+public class CenterDetail  extends BaseModel {
+	
+	// ATTRIBUTES :
+	
+	private Integer center;
+	private Integer nbMen;
+	private Integer nbWomen;
+	private Integer minAge;
+	private Integer maxAge;
+	
+	// CONSTRUCTORS :
+	public CenterDetail() {}
+	
+	public CenterDetail(Integer center, Integer nbMen, Integer nbWomen, Integer minAge, Integer maxAge) throws ModelException {
+		this.setCenter(center);
+		this.setNbMen(nbMen);
+		this.setNbWomen(nbWomen);
+		this.setMinAge(minAge);
+		this.setMaxAge(maxAge);
+	}
+	
+	public CenterDetail(Integer id, Integer center, Integer nbMen, Integer nbWomen, Integer minAge, Integer maxAge) throws ModelException {
+		this.setId(id);
+		this.setCenter(center);
+		this.setNbMen(nbMen);
+		this.setNbWomen(nbWomen);
+		this.setMinAge(minAge);
+		this.setMaxAge(maxAge);
+	}
+	
+	// GETTERS AND SETTERS :
+	
+	public Integer getCenter() {
+		return center;
+	}
+	public void setCenter(Integer center) throws ModelException {
+		if(center > 0)
+			this.center = center;
+		else
+			throw new ModelException("Invalid value on Center : " + center + " !");
+	}
+	public Integer getNbMen() {
+		return nbMen;
+	}
+	public void setNbMen(Integer nbMen) throws ModelException {
+		if(nbMen >= 0)
+			this.nbMen = nbMen;
+		else
+			throw new ModelException("Invalid value on nbMen : " + nbMen + " !");
+	}
+	public Integer getNbWomen() {
+		return nbWomen;
+	}
+	public void setNbWomen(Integer nbWomen) throws ModelException {
+		if(nbMen >= 0)
+			this.nbWomen = nbWomen;
+		else
+			throw new ModelException("Invalid value on nbWomen : " + nbWomen + " !");
+	}
+	public Integer getMinAge() {
+		return minAge;
+	}
+	public void setMinAge(Integer minAge) throws ModelException {
+		if(minAge >= 0)
+			this.minAge = minAge;
+		else
+			throw new ModelException("Invalid value on minAge : " + minAge + " !");
+	}
+	public Integer getMaxAge() {
+		return maxAge;
+	}
+	public void setMaxAge(Integer maxAge) throws ModelException {
+		if(maxAge >= 0)
+			this.maxAge = maxAge;
+		else
+			throw new ModelException("Invalid value on maxAge : " + maxAge + " !");
+	}
+	
+	
+}
