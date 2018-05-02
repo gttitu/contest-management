e1360a5d5823342b68848f3ed2a295118a13b58b
Gimmy <gimmyarazafimbelo2@gmail.com>
2018-05-02 14:09:17 +0300

    Fix bug and update interface dao

--- Projet-Naina-S6-Part2/src/gtt/test/Program.java
+++ Projet-Naina-S6-Part2/src/gtt/test/Program.java
@@ -2,11 +2,16 @@ package gtt.test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 import gtt.model.BaseModel;
+import gtt.model.candidate.Candidate;
+import gtt.model.candidate.CandidateDetail;
+import gtt.model.center.Room;
 import gtt.model.dao.Connection;
 import gtt.model.dao.generic.GenericDAO;
 
+@SuppressWarnings("unused")
 public class Program {
 	
 	public static void main(String[] args) {
@@ -17,16 +22,10 @@ public class Program {
 		GenericDAO dao = new GenericDAO(connection, true);
 		try {
 			
-			/*Voiture v = new Voiture();
-			v.setModele("Ter%");
-			List<Voiture> voitures = convertToVoiture(dao.findAll(v, null));
-			listVoitures(voitures);*/
-			
-			//Marque m = new Marque("Peugeot");
-			//dao.save(m);
-			
-			Voiture v = new Voiture(3, 2, "18");
-			dao.update(v);
+			//findAll(dao);
+			//save(dao);
+			//update(dao);
+			delete(dao);
 			
 		} catch (Exception e) {
 			
@@ -44,52 +43,52 @@ public class Program {
 	static java.sql.Connection initConnection() {
 		
 		String driver = "com.mysql.jdbc.Driver";
-		String url = "jdbc:mysql://127.0.0.1:3306/TestGeneric";
+		String url = "jdbc:mysql://127.0.0.1:3306/contest-management";
 		return  Connection.get(driver, url, "root", "root");
 		
 	}
 	
-	static List<Marque> convertToMarque(List<BaseModel> models){
+	static void findById(GenericDAO dao) throws Exception {
 		
-		List<Marque> result = new ArrayList<>();
+		Room model = new Room(); model.setId(2);
+		dao.findById(model);
 		
-		for(BaseModel m : models) {
-			
-			result.add((Marque)m);
-			
-		} return result;
+		System.out.println(model);
 		
 	}
 	
-	static List<Voiture> convertToVoiture(List<BaseModel> models){
+	@SuppressWarnings("unchecked")
+	static void findAll(GenericDAO dao) throws Exception {
 		
-		List<Voiture> result = new ArrayList<>();
-		
-		for(BaseModel m : models) {
-			
-			result.add((Voiture)m);
-			
-		} return result;
-		
-	}
-	
-	static void listVoitures(List<Voiture> voitures) {
-		
-		for(Voiture v : voitures) {
-			
-			System.out.println(v.getId() + " : " + v.getMarque() + " : " + v.getModele());
-			
-		}
+		List<Room> rooms = dao.findAll(2, 2, new Room(), null);
+		rooms.forEach(new Consumer<Room>() {
+
+			@Override
+			public void accept(Room t) {
+				System.out.println(t);
+			}
+		});
 		
 	}
 	
-	static void listMarques(List<Marque> marques) {
+	static void save(GenericDAO dao) throws Exception{
 		
-		for(Marque m : marques) {
-			
-			System.out.println(m.getId() + " : " + m.getNom());
-			
-		}
+		CandidateDetail model = new CandidateDetail(5, "Ryan", "Chan", 20, 1);
+		dao.save(model);
+		
+	}
+	
+	static void update(GenericDAO dao) throws Exception{
+		
+		Candidate model = new Candidate(6, 3);
+		dao.update(model);
+		
+	}
+	
+	static void delete(GenericDAO dao) throws Exception{
+		
+		Candidate model = new Candidate(6, 3);
+		dao.delete(model);
 		
 	}
 
