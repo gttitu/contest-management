c52f53496087ffd3b5937109ba5a7c67a3f10aed
Toavina <rabejaona.toavina@gmail.com>
2018-05-31 10:52:29 +0300

    correction de l erreur dans markservice

--- Projet-S6-Spring/src/gtt/test/DemoMark.java
+++ Projet-S6-Spring/src/gtt/test/DemoMark.java
@@ -31,10 +31,16 @@ public class DemoMark {
 	}
 	
 	static void addMark(MarkService serv) throws Exception {
-		Mark mark = new Mark(1,1,1, (float)12.5);
-		Matter matter = new Matter(1,1,"Droits",3,(float)25.00,"2018-05-02 08:00:00","2018-05-02 11:30:00");
 		Candidate candidate = new Candidate(1,1);
 		Contest contest = new Contest(1,"Concours des magistrats",false,"2018-05-02","2018-05-06");
-		serv.addMark(mark,matter,candidate,contest);		
+		
+		/*Mark mark = new Mark(1,1, (float)12.5);
+		Matter matter = new Matter(1,1,"Droits",3,(float)25.00,"2018-05-02 08:00:00","2018-05-02 11:30:00");
+		*/
+		
+		Mark mark1 = new Mark(1,2, (float)15.00);
+		Matter matter1 = new Matter(2,1,"Sciences politiques",4,(float)30.00,"2018-05-03 08:30:00","2018-05-03 11:30:00");
+		
+		serv.addMark(mark1,matter1,candidate,contest);		
 	}
 }
